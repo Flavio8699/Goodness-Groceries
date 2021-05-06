f059636139d19cd6fd7929b9f99eafa5442e948b
Flavio <flavio8699@gmail.com>
2021-05-06 14:44:30 +0200

    Add torch light on/off button

--- Code/GoodnessGroceries/Views/Scanner/Scanner.swift
+++ Code/GoodnessGroceries/Views/Scanner/Scanner.swift
@@ -9,14 +9,15 @@ struct Scanner: View {
     @EnvironmentObject var UserSettings: UserSettings
     @State var product: Product?
     @State var scannerIsActive: Bool = true
+    @State var torchLightIsActive: Bool = false
     @State var askPermissions: Bool = true
     
     var body: some View {
         NavigationView {
-            ZStack {
+            ZStack (alignment: .topLeading) {
                 CameraNotAllowedView()
                 if AVCaptureDevice.authorizationStatus(for: AVMediaType.video) == .authorized || !askPermissions {
-                    CBScanner(supportBarcode: .constant([.qr]), mockBarCode: .constant(BarcodeData(value: "2354896578839", type: .qr)), isActive: $scannerIsActive) { search in
+                    CBScanner(supportBarcode: .constant([.qr]), torchLightIsOn: $torchLightIsActive, mockBarCode: .constant(BarcodeData(value: "2354896578839", type: .qr)), isActive: $scannerIsActive) { search in
                         if let product = self.productsVM.products.first(where: { $0.code == search.value }) {
                             if self.product != product {
                                 self.product = product
@@ -27,7 +28,7 @@ struct Scanner: View {
                                 self.product = nil
                             }
                         }
-                    }
+                    }.edgesIgnoringSafeArea(.all)
                     if let product = self.product {
                         VStack {
                             Spacer()
@@ -37,10 +38,19 @@ struct Scanner: View {
                                     .cornerRadius(7)
                                     .shadow(color: Color.black.opacity(0.5), radius: 5, x: 0, y: 5)
                                     .padding(.horizontal, 20)
-                            }.padding(.bottom, 130)
+                            }.padding(.bottom, 35)
                             .navigationBarTitle(NSLocalizedString("BACK", lang: UserSettings.language))
                         }
                     }
+                    Button(action: {
+                        torchLightIsActive.toggle()
+                        impactFeedback(.medium)
+                    }, label: {
+                        ZStack {
+                            Circle().foregroundColor(.white).frame(width: 50, height: 50)
+                            Image(systemName: "flashlight.\(torchLightIsActive ? "on" : "off").fill").font(.system(size: 25, weight: .regular))
+                        }
+                    }).offset(x: 15, y: 15)
                 }
             }
             .JMModal(showModal: $askPermissions, for: [.camera], autoDismiss: true, autoCheckAuthorization: true)
@@ -53,7 +63,6 @@ struct Scanner: View {
             .onDisappear {
                 scannerIsActive = false
             }
-            .edgesIgnoringSafeArea(.all)
             .navigationBarTitle("", displayMode: .inline)
             .navigationBarHidden(true)
         }.navigationViewStyle(StackNavigationViewStyle())
